eb2ea13673b296270019bffa57e2cc4ce901b0d2
DayDay <daydaybigking@sina.com>
2020-10-22 14:59:18 +0800

    添加注释

--- index.js
+++ index.js
@@ -1,29 +1,45 @@
 class Carousel {
   constructor(options) {
+    // 如果没有传入 el, 则不作任何事情
     if (options.el) {
+      // 目标,用来获取 container、wrap、还有 items
       this.containerEl = options.el;
+      // 轮播组件可视范围宽度
       this.containerWidth = options.width || 800;
+      // 轮播内容间隔
       this.itemSpace = options.itemSpace || 20;
+      // 轮播方向
       this.direction = options.direction || "left";
+      // 单次轮播完成时间
       this.time = options.time || 50;
+      // container element
       this.container = document.querySelector(this.containerEl);
+      // wrap element
       this.wrap = document.querySelector(`${this.containerEl} .carousel-wrap`);
+      // item elements
       this.items = document.querySelectorAll(
         `${this.containerEl} .carousel-wrap .carousel-item`
       );
+      // 克隆 item 相关属性
       this.cloneChildWidth = 0;
       this.cloneCount = 0;
-
+      // init
       this.initContainer();
     } else {
       throw new Error("组件使用不规范, 请务必设定 el 属性!");
     }
   }
+  /**
+   * 给 container 设定样式, 并初始化 wrap
+   */
   initContainer() {
     this.container.style.width = this.containerWidth + "px";
     this.container.style.overflow = "hidden";
     this.initWrap();
   }
+  /**
+   * 先初始化 items, 然后设定 wrap 的宽度等样式, 并设定动画
+   */
   initWrap() {
     this.initItems();
     let length = 0;
@@ -56,11 +72,17 @@ class Carousel {
       this.generateStyleTag();
     }
   }
+  /**
+   * 给 items 设定样式, 并根据实际情况, 克隆若干元素作为循环交替元素
+   */
   initItems() {
     this.forEachHTMLCollection(this.items, (i) => (i.style.float = "left"));
     this.generateCloneItems();
   }
-  generateCloneItems(target) {
+  /**
+   * 根据 container 和 items 的宽度, 计算出需要克隆多少个元素成为循环交替元素
+   */
+  generateCloneItems() {
     let count = 0;
     let result = this.container.offsetWidth;
 
@@ -83,6 +105,9 @@ class Carousel {
     }
     this.cloneCount = count;
   }
+  /**
+   * 在 <head> 中添加 <style> 标签, 用来声明动画
+   */
   generateStyleTag() {
     const styleTag = document.createElement("style");
     styleTag.id = "move";
@@ -96,6 +121,11 @@ class Carousel {
 
     document.head.appendChild(styleTag);
   }
+  /**
+   * 遍历 HTMLCollection (document.querySelectorAll 的返回值)
+   * @param {HTMLCollection} collection 
+   * @param {Function} callback 
+   */
   forEachHTMLCollection(collection, callback) {
     for (let i = 0; i < collection.length; i++) {
       callback(collection[i]);
@@ -104,9 +134,9 @@ class Carousel {
 }
 
 new Carousel({
-  el: ".carousel-container",
-  direction: "left",
-  width: 800,
-  itemSpace: 20,
-  time: 25,
+  el: ".carousel-container", // 目标
+  direction: "left", // 轮播方向
+  width: 800, // 轮播可视范围宽度
+  itemSpace: 20, // 轮播内容间距
+  time: 25, // 单词轮播完成时间
 });
